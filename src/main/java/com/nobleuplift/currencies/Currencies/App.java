38daa6b7690ffc810e42d0b4d01700e5ed6298c7
Patrick Seiter <patrick.seiter@gmail.com>
2015-04-21 19:46:30 -0500

    Initial commit.

---
+++ src/main/java/com/nobleuplift/currencies/Currencies/App.java
@@ -0,0 +1,13 @@
+package com.nobleuplift.currencies.Currencies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
