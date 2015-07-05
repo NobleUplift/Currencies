d5d54f183c113500976788e6c5f4445c9456435b
Patrick Seiter <patrick.seiter@gmail.com>
2015-07-05 15:49:09 -0500

    Eclipse missed one

---
+++ src/main/java/com/nobleuplift/currencies/entities/TransactionType.java
@@ -0,0 +1,80 @@
+package com.nobleuplift.currencies.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+import java.util.List;
+
+
+/**
+ * The persistent class for the currencies_transaction_type database table.
+ * 
+ */
+@Entity
+@Table(name="currencies_transaction_type")
+@NamedQuery(name="TransactionType.findAll", query="SELECT t FROM TransactionType t")
+public class TransactionType implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(unique=true, nullable=false)
+	private Short id;
+
+	@Column(nullable=false, length=16)
+	private String name;
+
+	//bi-directional many-to-one association to Transaction
+	@OneToMany(mappedBy="type")
+	private List<Transaction> transactions;
+
+	public TransactionType() {
+	}
+
+	public Short getId() {
+		return this.id;
+	}
+
+	public void setId(Short id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "TransactionType [id=" + id + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TransactionType other = (TransactionType) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
