3b5f3a0be0a880202d4180600c9e9d309e8b2859
Patrick Seiter <patrick.seiter@gmail.com>
2015-07-05 12:30:10 -0500

    Eclipse won't allow me to stage these files...

---
+++ src/main/java/com/nobleuplift/currencies/entities/Holder.java
@@ -0,0 +1,99 @@
+package com.nobleuplift.currencies.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the currencies_holder database table.
+ * 
+ */
+@Entity
+@Table(name="currencies_holder")
+@NamedQuery(name="Holder.findAll", query="SELECT h FROM Holder h")
+public class Holder implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private HolderPK id;
+
+	@Column(nullable=false)
+	private short length;
+
+	//bi-directional many-to-one association to Account
+	@ManyToOne
+	@JoinColumn(name="child_account_id", nullable=false, insertable=false, updatable=false)
+	private Account childAccount;
+
+	//bi-directional many-to-one association to Account
+	@ManyToOne
+	@JoinColumn(name="parent_account_id", nullable=false, insertable=false, updatable=false)
+	private Account parentAccount;
+
+	public Holder() {
+	}
+
+	public HolderPK getId() {
+		return this.id;
+	}
+
+	public void setId(HolderPK id) {
+		this.id = id;
+	}
+
+	public short getLength() {
+		return this.length;
+	}
+
+	public void setLength(short length) {
+		this.length = length;
+	}
+
+	public Account getChildAccount() {
+		return this.childAccount;
+	}
+
+	public void setChildAccount(Account childAccount) {
+		this.childAccount = childAccount;
+	}
+
+	public Account getParentAccount() {
+		return this.parentAccount;
+	}
+
+	public void setParentAccount(Account parentAccount) {
+		this.parentAccount = parentAccount;
+	}
+
+	@Override
+	public String toString() {
+		return "Holder [id=" + id + ", length=" + length + ", childAccount="
+				+ childAccount + ", parentAccount=" + parentAccount + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Holder other = (Holder) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
---
+++ src/main/java/com/nobleuplift/currencies/entities/HolderPK.java
@@ -0,0 +1,57 @@
+package com.nobleuplift.currencies.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * The primary key class for the currencies_holder database table.
+ * 
+ */
+@Embeddable
+public class HolderPK implements Serializable {
+	//default serial version id, required for serializable classes.
+	private static final long serialVersionUID = 1L;
+
+	@Column(name="parent_account_id", insertable=false, updatable=false, unique=true, nullable=false)
+	private int parentAccountId;
+
+	@Column(name="child_account_id", insertable=false, updatable=false, unique=true, nullable=false)
+	private int childAccountId;
+
+	public HolderPK() {
+	}
+	public int getParentAccountId() {
+		return this.parentAccountId;
+	}
+	public void setParentAccountId(int parentAccountId) {
+		this.parentAccountId = parentAccountId;
+	}
+	public int getChildAccountId() {
+		return this.childAccountId;
+	}
+	public void setChildAccountId(int childAccountId) {
+		this.childAccountId = childAccountId;
+	}
+
+	public boolean equals(Object other) {
+		if (this == other) {
+			return true;
+		}
+		if (!(other instanceof HolderPK)) {
+			return false;
+		}
+		HolderPK castOther = (HolderPK)other;
+		return 
+			(this.parentAccountId == castOther.parentAccountId)
+			&& (this.childAccountId == castOther.childAccountId);
+	}
+
+	public int hashCode() {
+		final int prime = 31;
+		int hash = 17;
+		hash = hash * prime + this.parentAccountId;
+		hash = hash * prime + this.childAccountId;
+		
+		return hash;
+	}
+}
